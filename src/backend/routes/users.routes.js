f818aee4d51f81ba67651d17baa44d850bf02de6
Jaime Suarez <jaisua8@gmail.com>
2025-05-23 20:43:31 -0500

    express.jjss update

--- src/backend/routes/users.routes.js
+++ src/backend/routes/users.routes.js
@@ -6,4 +6,21 @@ router.get("/users", (req, res) => {
     res.send("Obteniendo Usuarios!");
 })
 
+router.get("/users/:id", (req, res) => {
+    const{id}=req.params;
+    res.send("Obteniendo el usuario "+id);
+})
+
+router.post("/users", (req, res) => {
+    res.send("Creando Usuarios!");
+})
+
+router.delete("/users/:id", (req, res) => {
+    res.send("Eliminando Usuarios!");
+})
+
+router.put("/users/:id", (req, res) => {
+    res.send("Actualizando Usuarios!");
+})
+
 export default router;
